4116a3451a421530c4fc0b05953f46d16a05efe9
Minsang Choi <radiofun8@gmail.com>
2026-05-30 07:37:15 -0700

    Initial Commit

---
+++ InkShader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InkShader
+//
+//  Created by Minsang Choi on 5/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InkShader/InkShaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkShaderApp.swift
+//  InkShader
+//
+//  Created by Minsang Choi on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct InkShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
